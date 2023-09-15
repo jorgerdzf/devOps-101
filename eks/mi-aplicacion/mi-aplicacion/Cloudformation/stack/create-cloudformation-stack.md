c5230d2eaf5187aac83a085f237235cde1518b15
Jorge Rodriguez <jorgerdzf@gmail.com>
2023-09-15 11:48:09 -0600

    updated pipeline template

--- eks/mi-aplicacion/mi-aplicacion/Cloudformation/stack/create-cloudformation-stack.md
+++ eks/mi-aplicacion/mi-aplicacion/Cloudformation/stack/create-cloudformation-stack.md
@@ -7,6 +7,12 @@ Just provide the required parameters to deploy to the environment you desire.
 The template.yml is an empty template so that you can create a stack that then will be linked to the pipeline. 
 Once the pipeline is working, the template.yml will be updated with the one that is in the project repository.
 
+## Important considerations
+
+Run on linux
+Be sure to have aws cli profile configured
+Be sure to have jq utility installed
+
 ## Create a connection to the repository
 
 You need to create a connection to the repository in order to link the pipeline to the repo.
@@ -23,7 +29,7 @@ The PIPELINE_CONNECTION_ARN is the arn of the connection you just created.
 | ROLE_POLICY_FILE        | Location of the trust policy file                                               | file://./pipeline/policies/stc-ImmsExchange-cloudformation-policy.json                                     |
 | API_STACK_TEMPLATE      | Location of the cloudformation template for the ImmsExchange API                | file://./api/initial-template.yml                                                                   |
 | PIPELINE_STACK_TEMPLATE | Location of the cloudformation template for the ImmsExchange pipeline           | file://./pipeline/pipeline.yml                                                                      |
-| PIPELINE_CONNECTION_ARN | ARN of the connection you just created as described in previous steps           | arn:aws:codestar-connections:us-west-2:474455999118:connection/4b0f8c65-5c27-459c-9477-bb87f7bb0114 |
+| PIPELINE_CONNECTION_ARN | ARN of the connection you just created as described in previous steps           | arn:aws:codestar-connections:us-east-2:356403663115:connection/68c0ec37-5bfa-40cb-935a-9e731b1faa61 |
 | REPOSITORY_ID           | User and repository name (<user>/<repository>) where the API is stored			| jorgerdzf/mi-aplicacion                                                                                 |
 | REPOSITORY_BRANCH       | Name of the branch that pipeline will be listening to be triggered              | master                                                                                                 |
 | ENVIRONMENT_TYPE        | Must be dev, qa, staging or prod                                                | test                                                                                                 |
@@ -35,14 +41,14 @@ The following script is an example to create the required stacks for a test envi
 ## Development Environment Script
 
 ```bash
-AWS_CLI_PROFILE="personal-jorge" \
-ROLE_TRUST_POLICY_FILE="file://./pipeline/policies/mi-aplicacion-cloudformation-trust-policy.json" \
-ROLE_POLICY_FILE="file://./pipeline/policies/mi-aplicacion-cloudformation-policy.json" \
-API_STACK_TEMPLATE="file://./api/initial-template.yml" \
-PIPELINE_STACK_TEMPLATE="file://./pipeline/pipeline.yml" \
-PIPELINE_CONNECTION_ARN="arn:aws:codestar-connections:us-west-2:474455999118:connection/4b0f8c65-5c27-459c-9477-bb87f7bb0114" \
-REPOSITORY_ID="jorgerdzf/mi-aplicacion" \
-REPOSITORY_BRANCH="master" \
+AWS_CLI_PROFILE="jorge" \
+ROLE_TRUST_POLICY_FILE="file://../pipeline/policies/mi-aplicacion-cloudformation-trust-policy.json" \
+ROLE_POLICY_FILE="file://../pipeline/policies/mi-aplicacion-cloudformation-policy.json" \
+API_STACK_TEMPLATE="file://../api/template.yml" \
+PIPELINE_STACK_TEMPLATE="file://../pipeline/pipeline.yml" \
+PIPELINE_CONNECTION_ARN="arn:aws:codestar-connections:us-east-2:356403663115:connection/68c0ec37-5bfa-40cb-935a-9e731b1faa61" \
+REPOSITORY_ID="jorgerdzf/devOps-101" \
+REPOSITORY_BRANCH="main" \
 ENVIRONMENT_TYPE="test" \
 ./create-cloudformation-stack.sh
 ```
--- eks/mi-aplicacion/mi-aplicacion/Cloudformation/stack/create-cloudformation-stack.sh
+++ eks/mi-aplicacion/mi-aplicacion/Cloudformation/stack/create-cloudformation-stack.sh
@@ -1,86 +1,161 @@
 #!/bin/bash
 
 create_cloudformation_stack () {
-  echo "Creating the needed cloudformation role"
+  printf "\n Creating the needed cloudformation role \n"
   roleName="mi-aplicacion-cloudformation-role-${ENVIRONMENT_TYPE}"
-  roleArn=$(aws --profile="${AWS_CLI_PROFILE}" iam create-role --role-name $roleName --assume-role-policy-document "${ROLE_TRUST_POLICY_FILE}" --output text --query 'Role.Arn')
-  
+
+  role_description=$(aws iam get-role --role-name "$roleName" 2>&1)
+
+  if [ $? -eq 0 ]; then
+    roleArn=$(echo "$role_description" | jq -r '.Role.Arn')
+    printf " Role $roleName exists with arn: $roleArn \n"
+  else
+    printf " Role $roleName does not exist, proceed to add it. \n"
+    roleArn=$(aws --profile="${AWS_CLI_PROFILE}" iam create-role --role-name $roleName --assume-role-policy-document "${ROLE_TRUST_POLICY_FILE}" --output text --query 'Role.Arn')
+    echo "The generated $roleName role arn is: $roleArn"
+    printf "\n waiting 10 seconds so that cloudformation recognizes the newly created role \n"
+    sleep 10
+  fi
+
   if [ -z "${roleArn}" ]; then
-    echo "Error while creating the role"
+    echo "Error while reading/creating the role"
     exit 1;
   fi
 
-  echo "The $roleName role arn is: $roleArn"
-  echo "Embed the permissions policy to the role to specify what it is allowed to do"
+  printf "\n Embed the permissions policy to the role to specify what it is allowed to do \n"
   aws --profile="${AWS_CLI_PROFILE}" iam put-role-policy \
   --role-name "$roleName" \
   --policy-name "mi-aplicacion-cloudformation-policy-${ENVIRONMENT_TYPE}" \
   --policy-document "${ROLE_POLICY_FILE}"
 
-  echo "waiting 10s so that cloudformation recognizes the newly created role"
-  sleep 10
+  #CHECK IF STACK IS ALREADY EXISTENT
+  stack_description=$(aws cloudformation describe-stacks --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" 2>&1)
 
-  echo "Creating the cloudformation stack and change set"
-  stackId=$(aws --profile="${AWS_CLI_PROFILE}" cloudformation create-change-set \
-  --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" \
-  --template-body "${API_STACK_TEMPLATE}" \
-  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-  --role-arn "$roleArn" \
-  --change-set-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}-changeset" \
-  --change-set-type CREATE \
-  --output text --query 'StackId')
+  if [ $? -eq 0 ]; then
+    printf "\n \n Stack mi-aplicacion-api-${ENVIRONMENT_TYPE} has already been created. \n"
+  else
+    #CLOUDFORMATION STACK CREATION
+    echo "Creating the cloudformation stack and change set"
+    stackId=$(aws --profile="${AWS_CLI_PROFILE}" cloudformation create-change-set \
+    --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" \
+    --template-body "${API_STACK_TEMPLATE}" \
+    --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    --parameters ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" \
+    --role-arn "$roleArn" \
+    --change-set-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}-changeset" \
+    --change-set-type CREATE \
+    --output text --query 'StackId')
 
-  if [ -z "${stackId}" ]; then
-    echo "Error while creating the cloudformation API stack"
-    exit 1;
+    while true; do
+      change_set_status=$(aws cloudformation describe-change-set --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" \
+        --change-set-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}-changeset" --query "Status" --output text)
+      if [ "$change_set_status" == "CREATE_COMPLETE" ]; then
+        break
+      elif [ "$change_set_status" == "FAILED" ]; then
+        echo "Cloudformation change set creation failed."
+        exit 1
+      else
+        echo "Waiting for cloudformation change set to be created"
+        sleep 5
+      fi
+    done
+
+    if [ -z "${stackId}" ]; then
+      echo "Error while creating the cloudformation API stack"
+      exit 1;
+    fi
+
+    printf "\n \n Cloudformation mi-aplicacion-api-${ENVIRONMENT_TYPE} stack created with the id $stackId \n"
+
+    #CLOUDFORMATION STACK EXECUTION
+    aws cloudformation execute-change-set --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" \
+      --change-set-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}-changeset"
+
+    #Wait until execution is complete
+    while true; do
+      stack_status=$(aws cloudformation describe-stacks --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" \
+        --query "Stacks[0].StackStatus" --output text)
+      if [ "$stack_status" == "CREATE_COMPLETE" ] || [ "$stack_status" == "UPDATE_COMPLETE" ]; then
+        break
+      elif [ "$stack_status" == "ROLLBACK_COMPLETE" ]; then
+        echo "Cloudformation stack execution has failed."
+        exit 1
+      else
+        echo "Waiting for cloudformation stack execution to complete"
+        sleep 5
+      fi
+    done
+
   fi
 
-  echo "Cloudformation mi-aplicacion-api-${ENVIRONMENT_TYPE} stack created with the id $stackId"
-
+  # We read the template outputs
   aws cloudformation describe-stacks --stack-name "mi-aplicacion-api-${ENVIRONMENT_TYPE}" > stack_info.json
 
   # Extract output values into a json
   repository_name=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="RepositoryName").OutputValue' stack_info.json)
   cluster_name=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="ClusterName").OutputValue' stack_info.json)
 
+  printf "\n Cloudformation Temple Output parameters: \n ECR Repository name: ${repository_name} \n EKS Cluster name: ${cluster_name}"
   # Remove temp json
   rm stack_info.json
 
+  #PIPELINE STACK CREATION
+  printf "\n\n Creating the cloudformation pipeline stack and change set \n"
 
-  echo "Creating the cloudformation pipeline stack and change set"
+  printf "\n Pipeline Parameters: \n EnvironmentType:=${ENVIRONMENT_TYPE} \n PipelineConnectionArn=${PIPELINE_CONNECTION_ARN} \n RepositoryID=${REPOSITORY_ID} \n RepositoryBranch=${REPOSITORY_BRANCH} \n CFRole=${roleArn}"
 
   pipelineChangeSetId=$(aws --profile="${AWS_CLI_PROFILE}" cloudformation create-change-set \
   --stack-name "mi-aplicacion-pipeline-${ENVIRONMENT_TYPE}" \
   --template-body "${PIPELINE_STACK_TEMPLATE}" \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-  --parameters ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" ParameterKey="PipelineConnectionArn",ParameterValue="${PIPELINE_CONNECTION_ARN}" ParameterKey="RepositoryId",ParameterValue="${REPOSITORY_ID}" ParameterKey="RepositoryBranch",ParameterValue="${REPOSITORY_BRANCH}" ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" ParameterKey="S3Bucket",ParameterValue="mi-aplicacion-${ENVIRONMENT_TYPE}-artifacts" ParameterKey="CFRole",ParameterValue="$roleArn" ParameterKey="RepositoryName",ParameterValue=$repository_name ParameterKey="ClusterName",ParameterValue=$cluster_name \
+  --parameters ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" ParameterKey="PipelineConnectionArn",ParameterValue="${PIPELINE_CONNECTION_ARN}" ParameterKey="RepositoryId",ParameterValue="${REPOSITORY_ID}" ParameterKey="RepositoryBranch",ParameterValue="${REPOSITORY_BRANCH}" ParameterKey="S3Bucket",ParameterValue="mi-aplicacion-${ENVIRONMENT_TYPE}-artifacts" ParameterKey="CFRole",ParameterValue="$roleArn" \
   --role-arn "$roleArn" \
   --change-set-name "mi-aplicacion-pipeline-${ENVIRONMENT_TYPE}-changeset" \
   --change-set-type CREATE \
   --output text --query 'Id')
+
+  #Wait for pipeline change set creation
+  while true; do
+    change_set_status=$(aws cloudformation describe-change-set --stack-name "mi-aplicacion-pipeline-${ENVIRONMENT_TYPE}" \
+      --change-set-name "mi-aplicacion-pipeline-${ENVIRONMENT_TYPE}-changeset" --query "Status" --output text)
+    if [ "$change_set_status" == "CREATE_COMPLETE" ]; then
+      break
+    elif [ "$change_set_status" == "FAILED" ]; then
+      printf "\n\n Pipeline change set creation failed."
+      exit 1
+    else
+      printf "\n\n Waiting for pipeline change set to be created"
+      sleep 5
+    fi
+  done
 
   if [ -z "${pipelineChangeSetId}" ]; then
     echo "Error while creating the pipeline stack"
     exit 1;
   fi
 
+  printf "\n Cloudformation mi-aplicacion-pipeline-${ENVIRONMENT_TYPE} stack created with the changeSet $pipelineChangeSetId"
 
-  echo "Cloudformation mi-aplicacion-pipeline-${ENVIRONMENT_TYPE} stack created with the changeSet $pipelineChangeSetId"
-
-  # It will poll every 30 seconds until a successful state has been reached
-  echo "Waiting for changeSet to be in status CREATE_COMPLETE"
-
-  aws --profile="${AWS_CLI_PROFILE}" cloudformation wait change-set-create-complete --change-set-name "$pipelineChangeSetId"
-
-  echo "Executing the change set of the mi-aplicacion-pipeline-${ENVIRONMENT_TYPE} stack"
+  #PIPELINE STACK EXECUTION
+  printf "Executing the change set of the mi-aplicacion-pipeline-${ENVIRONMENT_TYPE} stack"
 
   aws --profile="${AWS_CLI_PROFILE}" cloudformation execute-change-set --change-set-name "$pipelineChangeSetId"
 
-  # It will poll every 30 seconds until a successful state has been reached
-  echo "Waiting for changeSet to be executed"
+  # Wait until pipeline change set (CREATE_COMPLETE o UPDATE_COMPLETE)
+  while true; do
+    stack_status=$(aws cloudformation describe-stacks --stack-name "mi-aplicacion-pipeline-${ENVIRONMENT_TYPE}" \
+      --query "Stacks[0].StackStatus" --output text)
+    if [ "$stack_status" == "CREATE_COMPLETE" ] || [ "$stack_status" == "UPDATE_COMPLETE" ]; then
+      break
+    elif [ "$stack_status" == "ROLLBACK_COMPLETE" ]; then
+      echo "Pipeline stack execution has failed."
+      exit 1
+    else
+      printf "\n Waiting for pipeline stack execution to complete"
+      sleep 5
+    fi
+  done
 
-  aws --profile="${AWS_CLI_PROFILE}" cloudformation wait stack-create-complete --stack-name "mi-aplicacion-pipeline-${ENVIRONMENT_TYPE}"
-  
   echo "Change set execute completed"
 }
 
