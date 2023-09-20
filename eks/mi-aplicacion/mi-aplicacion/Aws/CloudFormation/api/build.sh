78492c85b0452597c6b3959a3d7104f45606377d
Jorge Rodriguez <jorgerdzf@gmail.com>
2023-09-20 14:38:43 -0600

    update build structure

--- eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/api/build.sh
+++ eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/api/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-build_and_deploy () {
-# INSTALL PHASE
+install () {
+    # INSTALL PHASE
     printf "Installing app dependencies... \n\n"
     curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl   
     chmod +x ./kubectl
     mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
     echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
     source ~/.bashrc
-    printf '\n Check kubectl & aws version: \n\n'
+    printf '\nCheck kubectl & aws version: \n\n'
     kubectl version --short --client
     aws --version
 
-# PRE-BUILD
-# EKS PART
-    printf "\n\n Logging into Amazon EKS...\n"
+    # PRE-BUILD
+    # EKS PART
+    printf "\n\nLogging into Amazon EKS...\n"
     echo "Caller identity:"
     aws sts get-caller-identity
 
@@ -30,28 +30,34 @@ build_and_deploy () {
     export AWS_EXPIRATION=$(echo ${CREDENTIALS} | jq -r '.Credentials.Expiration')
     
     # Setup kubectl with our EKS Cluster              
-    printf "\n\n Update Kube Config \n"      
+    printf "\n\nUpdate Kube Config \n"      
     aws eks update-kubeconfig --name $AWS_CLUSTER_NAME
 
-    printf "\n Check caller identity again \n"
+    printf "\nCheck caller identity again \n"
     aws sts get-caller-identity
-    
-    printf "\n\n Check config: \n"
+
+    printf "\n\nCheck config: \n"
     kubectl config view --minify
 
-    printf "\n\n Updating config map"
-    ROLE="    - rolearn: ${EKS_ROLE}\n      username: build\n      groups:\n        - system:masters"
-    kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"${ROLE}\";next}1" > /tmp/aws-auth-patch.yml
-    kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
-    
-    #- echo "- rolearn: $RoleArnParameter\n  username: <USERNAME>\n  groups:\n   system:masters" > add-role.yaml && kubectl apply -f add-role.yaml
-    echo check auth map config
-    kubectl describe configmap/aws-auth -n kube-system
     # Check access
-    echo check kubectl access
+    printf "\n\nCheck kubectl access \n"
     kubectl get svc
 
-# ECR PART
+    if [ $? -eq 0 ]; then
+        printf "\n\nCheck auth map config \n"
+        kubectl describe configmap/aws-auth -n kube-system
+        printf "\n\nUpdating config map"
+        ROLE="    - rolearn: ${EKS_ROLE}\n      username: build\n      groups:\n        - system:masters"
+        kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"${ROLE}\";next}1" > /tmp/aws-auth-patch.yml
+        kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
+        build
+    else
+        echo "Error while login to eks app"
+        exit 1
+    fi
+}
+build () {
+    # ECR PART
     echo Logging in to Amazon ECR...
     REPOSITORY_URI=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
     IMAGE_REPO_NAME=${APPLICATION_NAME}-repo-${ENVIRONMENT_TYPE}
@@ -59,14 +65,22 @@ build_and_deploy () {
     printf "\n REPOSITORY_URI=$REPOSITORY_URI \n IMAGE_REPO_NAME=$IMAGE_REPO_NAME \n IMAGE_TAG=$IMAGE_TAG \n\n"
     aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
-# BUILD PHASE
+    # BUILD PHASE
     echo Build started on `date`
     echo Building the Docker image...  
     cd eks/${APPLICATION_NAME}
     docker build -t $IMAGE_REPO_NAME:$IMAGE_TAG -f ${APPLICATION_NAME}/Dockerfile .
     docker tag $IMAGE_REPO_NAME:$IMAGE_TAG $REPOSITORY_URI/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-# DEPLOY PHASE
+    if [ $? -eq 0 ]; then
+        deploy
+    else
+        echo "Error while building app"
+        exit 1
+    fi
+}
+deploy () {
+    # DEPLOY PHASE
     echo Build completed on `date`
     echo Pushing the Docker image...
     docker push $REPOSITORY_URI/$IMAGE_REPO_NAME:$IMAGE_TAG
@@ -76,9 +90,8 @@ build_and_deploy () {
     kubectl rollout restart -f ${APPLICATION_NAME}/Aws/Kubernetes/${ENVIRONMENT_TYPE}/deployment.yaml
     kubectl get svc --all-namespaces
 }
-
 main(){
-  build_and_deploy
+  install
 }
 
 main "$@"
