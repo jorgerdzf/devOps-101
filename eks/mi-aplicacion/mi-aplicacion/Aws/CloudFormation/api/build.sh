161e8a9bef38ab42b4b2ac301f69523e0df16bd9
Jorge Rodriguez <jorgerdzf@gmail.com>
2023-09-20 14:12:17 -0600

    update bs

--- eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/api/build.sh
+++ eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/api/build.sh
@@ -2,6 +2,7 @@
 
 build_and_deploy () {
 # INSTALL PHASE
+    echo $(ls -l)
     echo Installing app dependencies...
     curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl   
     chmod +x ./kubectl
