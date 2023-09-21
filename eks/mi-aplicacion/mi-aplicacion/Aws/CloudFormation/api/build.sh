a54b461bb17401511e6f72b02aeb378e1e933138
Jorge Rodriguez <jorgerdzf@gmail.com>
2023-09-21 11:02:28 -0600

    cleanup deployment process

--- eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/api/build.sh
+++ eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/api/build.sh
@@ -2,41 +2,35 @@
 
 install () {
     # INSTALL PHASE
-    printf "Installing app dependencies... \n\n"
+    printf "\n\n Installing app dependencies... \n"
     curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl   
     chmod +x ./kubectl
     mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
     echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
     source ~/.bashrc
-    printf '\nCheck eksctl, kubectl & aws version: \n\n'
-    eksctl version --client
-    kubectl version --short --client
-    aws --version
+    printf '\n\nCHECK EKSCTL, KUBECTL & AWS VERSIONS: \n'
+    echo "EKSCTL VERSION: $(eksctl version)"
+    echo "KUBECTL VERSION: $(kubectl version --short --client)"
+    echo "AWS VERSION: $(aws --version)"
 
     # PRE-BUILD
     # EKS PART
     printf "\n\nLogging into Amazon EKS...\n"
-    echo "Caller identity:"
+    echo "CALLER IDENTITY:"
     aws sts get-caller-identity
 
-    # printf "Assuming eks role \n"
-    #aws sts assume-role --role-arn $EKS_ROLE --role-session-name test
-    echo "using cloudformation role"
-    CLOUDFORMATION_ROLE="arn:aws:iam::356403663115:role/mi-aplicacion-cloudformation-role-test"
-
+    #printf "\n\nASSUMING CLOUDFORMATION ROLE: \n"
     #echo "Extracting AWS Credential Information using STS Assume Role for kubectl"
     # printf "\n\n Setting Environment Variables related to AWS CLI for Kube Config Setup \n"          
-    CREDENTIALS=$(aws sts assume-role --role-arn ${CLOUDFORMATION_ROLE} --role-session-name codebuild-kubectl)
-    export AWS_ACCESS_KEY_ID="$(echo ${CREDENTIALS} | jq -r '.Credentials.AccessKeyId')"
-    export AWS_SECRET_ACCESS_KEY="$(echo ${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey')"
-    export AWS_SESSION_TOKEN="$(echo ${CREDENTIALS} | jq -r '.Credentials.SessionToken')"
-    export AWS_EXPIRATION=$(echo ${CREDENTIALS} | jq -r '.Credentials.Expiration')
+    # CREDENTIALS=$(aws sts assume-role --role-arn ${$CLOUDFORMATION_ROLE} --role-session-name codebuild-kubectl)
+    # export AWS_ACCESS_KEY_ID="$(echo ${CREDENTIALS} | jq -r '.Credentials.AccessKeyId')"
+    # export AWS_SECRET_ACCESS_KEY="$(echo ${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey')"
+    # export AWS_SESSION_TOKEN="$(echo ${CREDENTIALS} | jq -r '.Credentials.SessionToken')"
+    # export AWS_EXPIRATION=$(echo ${CREDENTIALS} | jq -r '.Credentials.Expiration')
     
-   
-
     # Setup kubectl with our EKS Cluster              
     printf "\n\nUpdate Kube Config \n"      
-    # aws eks update-kubeconfig --name $AWS_CLUSTER_NAME --role-arn $EKS_ROLE
+    # aws eks update-kubeconfig --name $AWS_CLUSTER_NAME --role-arn $CLOUDFORMATION_ROLE
     aws eks --region $AWS_DEFAULT_REGION update-kubeconfig --name $AWS_CLUSTER_NAME #--role-arn $CLOUDFORMATION_ROLE
 
     #export KUBECONFIG=$HOME/.kube/config
@@ -50,36 +44,28 @@ install () {
     printf "\n\nCheck iam identity mapping: \n"
     eksctl get iamidentitymapping --cluster $AWS_CLUSTER_NAME --region=$AWS_DEFAULT_REGION
 
-    printf "\n\Create config map: \n"
+    printf "\n\Add identity mappings: \n"
     eksctl create iamidentitymapping --cluster $AWS_CLUSTER_NAME --region=$AWS_DEFAULT_REGION \
     --arn $EKS_ROLE --username admin --group system:masters \
     --no-duplicate-arns
 
     if [ $? -eq 0 ]; then   
-        
-        build
+        # Check config maps
+        printf "\n\nCheck auth map config \n"
+        kubectl describe configmap/aws-auth -n kube-system
+
+        # Check access
+        printf "\n\nCheck kubectl access \n"
+        kubectl get svc
+
+        if [ $? -eq 0 ]; then
+            build
+        else
+            printf "\n\n ERROR WHILE LOGIN \n\n"
+            exit 1
+        fi
     else
         printf "\n\n ERROR CREATING IDENTITY MAPPING \n\n"
-        exit 1
-    fi
-
-    printf "\n\nCheck auth map config \n"
-    kubectl describe configmap/aws-auth -n kube-system
-    printf "\n\nUpdating config map"
-    
-    # ROLE="    - rolearn: ${EKS_ROLE}\n      username: build\n      groups:\n        - system:masters"
-    # kubectl get -n kube-system configmap/aws-auth -o yaml | awk "/mapRoles: \|/{print;print \"${ROLE}\";next}1" > /tmp/aws-auth-patch.yml
-    # kubectl patch configmap/aws-auth -n kube-system --patch "$(cat /tmp/aws-auth-patch.yml)"
-
-    # Check access
-    printf "\n\nCheck kubectl access \n"
-    
-    kubectl get svc
-
-    if [ $? -eq 0 ]; then
-        build
-    else
-        printf "\n\n ERROR WHILE LOGIN \n\n"
         exit 1
     fi
 }
@@ -108,14 +94,18 @@ build () {
 }
 deploy () {
     # DEPLOY PHASE
-    echo Build completed on `date`
-    echo Pushing the Docker image...
+    printf "\n\n BUILD COMPLETED ON `date` \n"
+    printf "\n\n PUSHING DOCKER IMAGE TO ECR... `date` \n"
     docker push $REPOSITORY_URI/$IMAGE_REPO_NAME:$IMAGE_TAG
-    echo Push the latest image to cluster
+    printf "\n\n PUSHING IMAGE TO EKS... `date` \n"
     kubectl apply -f ${APPLICATION_NAME}/Aws/Kubernetes/${ENVIRONMENT_TYPE}/deployment.yaml
     kubectl apply -f ${APPLICATION_NAME}/Aws/Kubernetes/${ENVIRONMENT_TYPE}/service.yaml
     kubectl rollout restart -f ${APPLICATION_NAME}/Aws/Kubernetes/${ENVIRONMENT_TYPE}/deployment.yaml
+
+    printf "\n\n CHECK CLUSTER COINFIGS `date` \n"
     kubectl get svc --all-namespaces
+    kubectl get services
+    kubectl get deployments
     kubectl get nodes
     kubectl cluster-info
 }
--- eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/stack/create-cloudformation-stack.sh
+++ eks/mi-aplicacion/mi-aplicacion/Aws/CloudFormation/stack/create-cloudformation-stack.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
 create_cloudformation_stack () {
-  printf "\n Creating the needed cloudformation role \n"
-  roleName="${APPLICATION_NAME}-cloudformation-role-${ENVIRONMENT_TYPE}"
-
-  role_description=$(aws iam get-role --role-name "$roleName" 2>&1)
+  printf "\nCreating the needed cloudformation role \n"
+  CLOUDFORMATION_ROLE_NAME="${APPLICATION_NAME}-cloudformation-role-${ENVIRONMENT_TYPE}"
+  CLOUDFORMATION_ROLE_DESCRIPTION=$(aws iam get-role --role-name "$CLOUDFORMATION_ROLE_NAME" 2>&1)
 
   if [ $? -eq 0 ]; then
-    roleArn=$(echo "$role_description" | jq -r '.Role.Arn')
-    printf " Role $roleName exists with arn: $roleArn \n"
+    CLOUDFORMATION_ROLE_ARN=$(echo "$CLOUDFORMATION_ROLE_DESCRIPTION" | jq -r '.Role.Arn')
+    printf "Role $CLOUDFORMATION_ROLE_NAME exists with arn: $CLOUDFORMATION_ROLE_ARN \n"
   else
-    printf " Role $roleName does not exist, proceed to add it. \n"
-    roleArn=$(aws --profile="${AWS_CLI_PROFILE}" iam create-role --role-name $roleName --assume-role-policy-document "${ROLE_TRUST_POLICY_FILE}" --output text --query 'Role.Arn')
-    echo "The generated $roleName role arn is: $roleArn"
-    printf "\n waiting 10 seconds so that cloudformation recognizes the newly created role \n"
+    printf "Role $CLOUDFORMATION_ROLE_NAME does not exist, proceed to add it. \n"
+    CLOUDFORMATION_ROLE_ARN=$(aws --profile="${AWS_CLI_PROFILE}" iam create-role --role-name $CLOUDFORMATION_ROLE_NAME --assume-role-policy-document "${ROLE_TRUST_POLICY_FILE}" --output text --query 'Role.Arn')
+    echo "The generated $CLOUDFORMATION_ROLE_NAME role arn is: $CLOUDFORMATION_ROLE_ARN"
+    printf "\nWaiting 10 seconds so that cloudformation recognizes the newly created role \n"
     sleep 10
   fi
 
-  if [ -z "${roleArn}" ]; then
+  if [ -z "${CLOUDFORMATION_ROLE_ARN}" ]; then
     echo "Error while reading/creating the role"
     exit 1;
   fi
 
-  printf "\n Embed the permissions policy to the role to specify what it is allowed to do \n"
+  printf "\nEmbed the permissions policy to the role to specify what it is allowed to do \n"
   aws --profile="${AWS_CLI_PROFILE}" iam put-role-policy \
-  --role-name "$roleName" \
+  --role-name "$CLOUDFORMATION_ROLE_NAME" \
   --policy-name "${APPLICATION_NAME}-cloudformation-policy-${ENVIRONMENT_TYPE}" \
   --policy-document "${ROLE_POLICY_FILE}"
 
@@ -41,7 +40,7 @@ create_cloudformation_stack () {
     --template-body "${API_STACK_TEMPLATE}" \
     --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
     --parameters ParameterKey="ApplicationName",ParameterValue=${APPLICATION_NAME} ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" \
-    --role-arn "$roleArn" \
+    --role-arn "$CLOUDFORMATION_ROLE_ARN" \
     --change-set-name "${APPLICATION_NAME}-api-${ENVIRONMENT_TYPE}-changeset" \
     --change-set-type CREATE \
     --output text --query 'StackId')
@@ -65,7 +64,7 @@ create_cloudformation_stack () {
       exit 1;
     fi
 
-    printf "\n \n Cloudformation ${APPLICATION_NAME}-api-${ENVIRONMENT_TYPE} stack created with the id $stackId \n"
+    printf "\n\nCloudformation ${APPLICATION_NAME}-api-${ENVIRONMENT_TYPE} stack created with the id $stackId \n"
 
     #CLOUDFORMATION STACK EXECUTION
     aws cloudformation execute-change-set --stack-name "${APPLICATION_NAME}-api-${ENVIRONMENT_TYPE}" \
@@ -94,27 +93,33 @@ create_cloudformation_stack () {
   # Extract output values into a json
   REPOSITORY_NAME=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="RepositoryName").OutputValue' stack_info.json)
   CLUSTER_NAME=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="ClusterName").OutputValue' stack_info.json)
-  CODEPIPELINE_ARN=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="CodePipelineRoleArn").OutputValue' stack_info.json)
-  CODEBUILD_ARN=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="CodeBuildRoleArn").OutputValue' stack_info.json)
+  #CODEPIPELINE_ARN=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="CodePipelineRoleArn").OutputValue' stack_info.json)
+  #CODEBUILD_ARN=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="CodeBuildRoleArn").OutputValue' stack_info.json)
   EKS_ARN=$(jq -r '.Stacks[0].Outputs[] | select(.OutputKey=="EksClusterRoleArn").OutputValue' stack_info.json)
 
-  printf "\n Cloudformation Temple Output parameters: \n ECR Repository name: ${REPOSITORY_NAME} \n EKS Cluster name: ${CLUSTER_NAME}"
+  printf "\nCodePipeline Input Parameters: \n \
+  Application Name: ${APPLICATION_NAME} \
+  Environment Type: ${ENVIRONMENT_TYPE} \
+  Pipeline Source Connection ARN: ${PIPELINE_CONNECTION_ARN} \
+  ECR Repository Id: ${REPOSITORY_NAME} \
+  Code Repository Branch: ${REPOSITORY_BRANCH} \
+  S3 Bucket: ${APPLICATION_NAME}-${ENVIRONMENT_TYPE}-artifacts \
+  EKS Cluster name: ${CLUSTER_NAME} \
+  CloudFormation Role ARN=${CLOUDFORMATION_ROLE_ARN} \
+  Cluster Role ARN: ${EKS_ARN}"
+
   # Remove temp json
   rm stack_info.json
 
   #PIPELINE STACK CREATION
-  printf "\n\n Creating the cloudformation pipeline stack and change set \n"
-  printf "\n Pipeline Parameters: \n ApplicationName=${APPLICATION_NAME} \n EnvironmentType=${ENVIRONMENT_TYPE} \n"
-  printf "PipelineConnectionArn=${PIPELINE_CONNECTION_ARN} \n RepositoryID=${REPOSITORY_ID} \n"
-  printf "RepositoryBranch=${REPOSITORY_BRANCH} \n CFRole=${roleArn} \n EKSClusterName=${CLUSTER_NAME} \n"
-  printf "CodeBuildRoleArn=${CODEBUILD_ARN} \n CodePipelineRoleArn=${CODEPIPELINE_ARN} \n EksRoleArn=${EKS_ARN} \n"
+  printf "\n\n Creating the cloudformation pipeline stack and change set:"
 
   pipelineChangeSetId=$(aws --profile="${AWS_CLI_PROFILE}" cloudformation create-change-set \
   --stack-name "${APPLICATION_NAME}-pipeline-${ENVIRONMENT_TYPE}" \
   --template-body "${PIPELINE_STACK_TEMPLATE}" \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-  --parameters ParameterKey="ApplicationName",ParameterValue=${APPLICATION_NAME} ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" ParameterKey="PipelineConnectionArn",ParameterValue="${PIPELINE_CONNECTION_ARN}" ParameterKey="RepositoryId",ParameterValue="${REPOSITORY_ID}" ParameterKey="RepositoryBranch",ParameterValue="${REPOSITORY_BRANCH}" ParameterKey="S3Bucket",ParameterValue="${APPLICATION_NAME}-${ENVIRONMENT_TYPE}-artifacts" ParameterKey="CFRole",ParameterValue="$roleArn" ParameterKey="EKSClusterName",ParameterValue="$CLUSTER_NAME" ParameterKey="CodePipelineRoleArn",ParameterValue="$CODEPIPELINE_ARN" ParameterKey="CodeBuildRoleArn",ParameterValue="$CODEBUILD_ARN" ParameterKey="EksClusterRoleArn",ParameterValue="$EKS_ARN"\
-  --role-arn "$roleArn" \
+  --parameters ParameterKey="ApplicationName",ParameterValue=${APPLICATION_NAME} ParameterKey="EnvironmentType",ParameterValue="${ENVIRONMENT_TYPE}" ParameterKey="PipelineConnectionArn",ParameterValue="${PIPELINE_CONNECTION_ARN}" ParameterKey="RepositoryId",ParameterValue="${REPOSITORY_ID}" ParameterKey="RepositoryBranch",ParameterValue="${REPOSITORY_BRANCH}" ParameterKey="S3Bucket",ParameterValue="${APPLICATION_NAME}-${ENVIRONMENT_TYPE}-artifacts" ParameterKey="CFRole",ParameterValue="$CLOUDFORMATION_ROLE_ARN" ParameterKey="EKSClusterName",ParameterValue="$CLUSTER_NAME" ParameterKey="EksClusterRoleArn",ParameterValue="$EKS_ARN"\
+  --role-arn "$CLOUDFORMATION_ROLE_ARN" \
   --change-set-name "${APPLICATION_NAME}-pipeline-${ENVIRONMENT_TYPE}-changeset" \
   --change-set-type CREATE \
   --output text --query 'Id')
